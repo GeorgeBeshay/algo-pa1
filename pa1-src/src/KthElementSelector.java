bdbdd707cf8f299dd43eabbc000f4d5d2d01def1
George Beshay <georgebeshay101@gmail.com>
2023-11-06 13:08:03 +0200

    Initializing Coding Environment.

---
+++ pa1-src/src/KthElementSelector.java
@@ -0,0 +1,29 @@
+public class KthElementSelector {
+    /*
+     Kth Element Selection Problem:
+     Given a collection of unsorted elements (possibly an array), it is required to find the kth smallest element
+     among this collection such that, when k = 0, the element is the minimum element in the collection, and when
+     k = collection.length() - 1, the element is the maximum element in the collection.
+     */
+
+    /*
+    # TODO: Comment randomizedApproach()
+     */
+    public static int randomizedApproach(int[] elements, int k){
+        return 0;
+    }
+
+    /*
+    # TODO: Comment deterministicApproach()
+     */
+    public static int deterministicApproach(int[] elements, int k){
+        return 0;
+    }
+
+    /*
+    # TODO: Comment naiveApproach()
+     */
+    public static int naiveApproach(int[] elements, int k){
+        return 0;
+    }
+}
---
+++ pa1-src/src/MaxSideLength.java
@@ -0,0 +1,14 @@
+public class MaxSideLength {
+    /*
+    Given a set of two-dimensional points, the goal is to compute the maximum side length  of the
+    square that can be drawn around each point, such that no two squares intersect, except at the sides.
+    Notes:
+    - Each point will be at the center of the surrounding square.
+    - The sides of the squares can only be horizontal and vertical.
+     */
+
+    // TODO: Comment solve()
+    public long solve(String inputFile){
+        return 0;
+    }
+}
